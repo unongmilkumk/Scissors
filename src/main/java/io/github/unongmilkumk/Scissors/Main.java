689081515ed2185b97cf8cbc522fa2dafafbdfcb
유농밀크 <91447270+unongmilkumk@users.noreply.github.com>
2023-08-31 11:35:33 +0900

    RollBack Main.java

---
+++ src/main/java/io/github/unongmilkumk/Scissors/Main.java
@@ -0,0 +1,12 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Scissors API Plugin Enabled")
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("Scissors API Plugin Disabled")
+    }
+}
