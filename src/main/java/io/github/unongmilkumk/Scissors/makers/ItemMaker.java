8bc737df6fb819d4ff9e59057d65f4f59b93cd89
MinecraftUserHAN1110 <93987816+MinecraftUserHAN1110@users.noreply.github.com>
2023-09-02 23:44:37 +0900

    Updated
    - Added Lombok Hooking
    - Fixed GuiMaker Bug
    - Added ItemMaker Durability System
    - Added Vector Mathmatics Calculator

--- src/main/java/io/github/unongmilkumk/Scissors/makers/ItemMaker.java
+++ src/main/java/io/github/unongmilkumk/Scissors/makers/ItemMaker.java
@@ -1,5 +1,6 @@
 package io.github.unongmilkumk.Scissors.makers;
 
+import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
@@ -9,7 +10,8 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.List;
 
 public class ItemMaker {
-    public final ItemStack item;
+    @Getter
+    private final ItemStack item;
     public ItemMaker(Material material) {
         this.item = new ItemStack(material);
     }
@@ -42,6 +44,11 @@ public class ItemMaker {
         ItemMeta im = item.getItemMeta();
         im.addItemFlags(itemFlags);
         item.setItemMeta(im);
+        return this;
+    }
+
+    public ItemMaker damage(short damage) {
+        item.setDurability(damage);
         return this;
     }
 }
